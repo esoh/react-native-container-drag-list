021bc38e2f6cea6bb133e113a759575e77226dd2
esoh <sean.so.oh@gmail.com>
2022-03-17 20:30:19 -0700

    fix dragging behavior

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-native-container-drag-list
+
+Make sure you remove `node_modules` in the root when running the `example`!
+
+`npm publish`
--- src/dragHandlers/LongPressDragHandler.tsx
+++ src/dragHandlers/LongPressDragHandler.tsx
@@ -1,45 +1,57 @@
 import React, {useRef} from 'react';
-import { State, LongPressGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, { useSharedValue, useAnimatedGestureHandler } from 'react-native-reanimated';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import Animated, { useSharedValue, runOnJS } from 'react-native-reanimated';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 
 function LongPressDragHandler({ dragProps = {}, children }: { dragProps: any, children: any }) {
   const { onDrag, onDragStart, onDragEnd } = dragProps;
 
+  const isPanning = useSharedValue(false);
   const y0 = useSharedValue(null);
-  const eventHandler = useAnimatedGestureHandler({
-    onActive: e => {
+
+  const onDragStartInternal = (y: number) => {
+    ReactNativeHapticFeedback.trigger('impactLight');
+    if (onDragStart) onDragStart(y);
+  };
+
+  const onDragEndInternal = (y: number) => {
+    if (onDragEnd) onDragEnd(y);
+  };
+
+  const panGesture = Gesture.Pan()
+    .onBegin((e) => {
+      isPanning.value = true;
+    })
+    .onUpdate((e) => {
       if (y0.value === null) return;
       if (onDrag) onDrag(e.absoluteY - y0.value, e.absoluteY);
-    },
-    onFinish: () => {
-      y0.value = null;
-    },
-  });
+    })
+    .onEnd((e) => {
+      if (y0.value !== null) {
+        runOnJS(onDragEndInternal)(e.absoluteY);
+        y0.value = null;
+      }
+      isPanning.value = true;
+    });
 
-  const panRef = useRef<PanGestureHandler>(null);
+  const longPressGesture = Gesture.LongPress()
+    .onStart((e) => {
+      y0.value = e.absoluteY;
+      runOnJS(onDragStartInternal)(e.absoluteY);
+    })
+    .onEnd((e) => {
+      if (!isPanning.value && y0.value !== null) {
+        runOnJS(onDragEndInternal)(e.absoluteY);
+        y0.value = null;
+      }
+    });
 
   return (
-    <LongPressGestureHandler
-      onHandlerStateChange={({ nativeEvent: { state, absoluteY } }) => {
-        'worklet';
-
-        if (state === State.ACTIVE) {
-          ReactNativeHapticFeedback.trigger('impactLight');
-          if (onDragStart) onDragStart(absoluteY);
-          y0.value = absoluteY;
-        } else {
-          if (onDragEnd) onDragEnd();
-          y0.value = null;
-        }
-      }}
-      // @ts-ignore
-      onGestureEvent={eventHandler}
-    >
+    <GestureDetector gesture={Gesture.Simultaneous(panGesture, longPressGesture)}>
       <Animated.View>
         {children}
       </Animated.View>
-    </LongPressGestureHandler>
+    </GestureDetector>
   );
 }
 
